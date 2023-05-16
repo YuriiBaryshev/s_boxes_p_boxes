443b641007025c5931162673fb4860a75204a8dc
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-16 20:04:32 +0300

    add sBox.encryptByte positive test cases

--- test/s_boxes_p_boxes_test.dart
+++ test/s_boxes_p_boxes_test.dart
@@ -2,15 +2,17 @@ import 'package:s_boxes_p_boxes/s_boxes_p_boxes.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('AES S-box tests', () {
+    final AESSBox sBox = AESSBox();
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('encrypts byte of data', () {
+      expect(sBox.encryptByte(0), 0x63);
+      expect(sBox.encryptByte(255), 0x16);
+      expect(sBox.encryptByte(0x77), 0xf5);
     });
   });
 }
